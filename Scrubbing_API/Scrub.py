808b860d71dcc8d968eb24011e7133286d9b1aa6
Justin Grenfell <justin.grenfell110@gmail.com>
2019-05-01 20:00:00 +0200

    Added Severity Index
    Severity Index shows how bad the personal info is on a scale of 1 being not too bad such as a name and 3 being you cant send the message such as passwords

--- Scrubbing_API/Scrub.py
+++ Scrubbing_API/Scrub.py
@@ -10,22 +10,42 @@ def scrub():
 		response = check(input_data)
 		return response
 
-#@return: a string containing word indexes(as if the string was an array) which reveal private info
+#@return: a string containing word indexes(as if the string was an array) which reveal private info 
+# followed by a ':' and then the severity of the PI with 3 as cant send and 1 as ok but weird o.O
 def check(input_data):	#Reads json file of predefined triggers and checks input against them, to be replaced with word2vec/Neural Network
 	with open('flags.txt', 'r') as file:
 		data_raw = file.read()
 	data = json.loads(data_raw)
 	result = set()
+	severity_index = []
 
 	input_data = input_data.lower()
 	for x in data["identifiers"]:
+		severity_count = 0
 		for y in data["privacy_type"]:
 			check_for = y + x
 			check_for = check_for.lower()
 			if check_for in input_data:
-				result.add(wordIndex(input_data, input_data.index(check_for) + len(check_for)))
+				temp = wordIndex(input_data, input_data.index(check_for) + len(check_for))
+				if temp not in result:
+					severity_index.append(data["severity_index"][severity_count])
+				result.add(temp)
+				
+			severity_count += 1
 	
-	result = repr(sorted(result))
+	result = repr(result)
+	severity_count = 0
+	i = 0
+	while i < len(result):
+		print(i)
+		if result[i] == ',' or result[i] == '}':
+			print(result)
+			print(str(severity_index[severity_count]))
+			print(result[0:i])
+			result = result[0: i] + ":" + str(severity_index[severity_count]) + result[i:]
+			severity_count += 1
+			i += 2
+		i += 1
 	return result
 
 def wordIndex(input_data, char_index):
