f2e8293701052714f0f9ca8bbbe4e2e7349c0f47
Justin Grenfell <justin.grenfell110@gmail.com>
2019-05-01 19:16:52 +0200

    Scrubbing API basics
    Scrubbing API basics done, will flag based on the values in the flags.txt file

--- Scrubbing_API/Scrub.py
+++ Scrubbing_API/Scrub.py
@@ -1,17 +1,46 @@
-# To run the program on windows open cmd and use the following commands
-# cd Desktop/"301 - Botic"/Botic/Scrubbing_API/venv/Scripts
-# activate
-# cd ../../
-# set FLASK_ENV=development
-# set FLASK_APP=Scrub.py
-
-from flask import Flask, request
+from flask import Flask, request, json
+import re
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return 'Server Works!'
-  
-@app.route('/greet')
-def say_hello():
-  return 'Hello from Server'
+@app.route('/scrub', methods=['GET', 'POST'])
+def scrub():
+	data = []
+	if request.method == 'POST' or request.method == 'GET':
+		input_data = request.form.get('data')
+		response = check(input_data)
+		return response
+
+#@return: a string containing word indexes(as if the string was an array) which reveal private info
+def check(input_data):	#Reads json file of predefined triggers and checks input against them, to be replaced with word2vec/Neural Network
+	with open('flags.txt', 'r') as file:
+		data_raw = file.read()
+	data = json.loads(data_raw)
+	result = set()
+
+	input_data = input_data.lower()
+	for x in data["identifiers"]:
+		for y in data["privacy_type"]:
+			check_for = y + x
+			check_for = check_for.lower()
+			if check_for in input_data:
+				result.add(wordIndex(input_data, input_data.index(check_for) + len(check_for)))
+	
+	result = repr(sorted(result))
+	return result
+
+def wordIndex(input_data, char_index):
+	input_data += ' '
+	count = char_index + 1
+	personal_info = ''
+	while True:
+		if input_data[count] == ' ':
+			count = 0
+			words = input_data.split(' ')
+			for x in words:
+				if personal_info != x:
+					count += 1
+				else:
+					return count
+		else:
+			personal_info += input_data[count]
+			count += 1
