159a7c06896087acf0b60afa31dfe54d1d8209ca
LesegoGomolemo <lesegomabe@ymail.com>
2019-05-05 00:18:17 +0200

    Finished deployment.

--- ai-message_scrubber/ms-travis.sh
+++ ai-message_scrubber/ms-travis.sh
@@ -1,22 +1,26 @@
-#!/bin/sh
-#get the service id of our application
-docker service ls
-#view services associated with the message-scrubber stack
-docker stack services message-scrubber
-#view the tasks of service
-docker service ps message-scrubber_web
-#view all tasks of stack
-docker stack ps message-scrubber
-#take down the app
-docker stack rm message-scrubber
-#take down the swarm
-docker swarm leave --force
-#
-#
+# !/bin/sh
+# get the service id of our application
+# docker service ls
+# view services associated with the message-scrubber stack
+# docker stack services message-scrubber
+# view the tasks of service
+# docker service ps message-scrubber_web
+# view all tasks of stack
+# docker stack ps message-scrubber
+# take down the app
+# docker stack rm message-scrubber
+# take down the swarm
+# docker swarm leave --force
+# 
+# 
 cd $TRAVIS_BUILD_DIR/ai-message_scrubber
 cat password.txt | docker login --username=_ --password-stdin registry.heroku.com
-#docker build -t registry.heroku.com/cryptic-hollows-60139/web .
-#docker push registry.heroku.com/cryptic-hollows-60139/web
-
-docker tag alabamaliquidservices/botic:latest-ms registry.heroku.com/cryptic-hollows-60139/web 
+# honestly, what's the point of building it again when it exists in the repo? 80/20
+docker build -t registry.heroku.com/cryptic-hollows-60139/web .
 docker push registry.heroku.com/cryptic-hollows-60139/web
+
+# release
+heroku container:release web
+
+# docker tag alabamaliquidservices/botic:latest-ms registry.heroku.com/cryptic-hollows-60139/web 
+# docker push registry.heroku.com/cryptic-hollows-60139/web
