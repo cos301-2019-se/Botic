1b4e7f645ede191961401b0077a0a608a8a5cac2
LesegoGomolemo <lesegomabe@ymail.com>
2019-05-14 19:32:50 +0200

    Travis Fix.

--- allisn/allisn-travis.sh
+++ allisn/allisn-travis.sh
@@ -1,2 +1,12 @@
 # !/bin/sh
-cd $TRAVIS_BUILD_DIR/allisn
+#cd $TRAVIS_BUILD_DIR/allisn
+heroku container:login
+#
+docker tag alabamaliquidservices/botic:latest-frontend registry.heroku.com/botic-frontend/web
+docker push registry.heroku.com/botic-frontend/web
+heroku container:release web --app botic-frontend
+#
+#template
+#
+#docker tag <image> registry.heroku.com/<app>/<process-type>
+#docker push registry.heroku.com/<app>/<process-type>
