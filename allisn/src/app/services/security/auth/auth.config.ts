61991fdc758bc293343067254d8b18731dad88a6
LesegoGomolemo <lesegomabe@ymail.com>
2019-05-20 05:54:02 +0200

    Corrected the redirect url.

--- allisn/src/app/services/security/auth/auth.config.ts
+++ allisn/src/app/services/security/auth/auth.config.ts
@@ -1,5 +1,3 @@
-//import { ENV } from './../core/env.config';
-
 interface AuthConfig {
     CLIENT_ID: string;
     CLIENT_DOMAIN: string;
@@ -12,6 +10,6 @@ export const AUTH_CONFIG: AuthConfig = {
     CLIENT_ID: 'gMNTaDHZ4Kat3JO8zMlpMwhqt1vvbk5V',
     CLIENT_DOMAIN: 'learning-lsg.auth0.com',
     AUDIENCE: 'botic-frontend.herokuapp.com/chat',
-    REDIRECT: /*`${ENV.BASE_URI}/callback`*/'http://localhost:4200/chat',
+    REDIRECT: 'https://botic-frontend.herokuapp.com/chat'/*'http://localhost:4200/chat'*/,
     SCOPE: 'openid profile read:profile send:messages read:messages',
 };
