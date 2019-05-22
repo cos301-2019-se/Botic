4f10abc27f5dea921399eab949d064dab922d574
Peter <u13042352@gmail.com>
2019-05-22 22:35:20 +0200

    Added an angular service for API call debugging and unit testing.

--- allisn/src/app/app.module.ts
+++ allisn/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { FormsModule } from '@angular/forms';
 import { TextScraperComponent } from './text-scraper/text-scraper.component';
 import { SafePipe } from './safe.pipe';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { TextInputHighlightModule } from 'angular-text-input-highlight';
 import { BotChatComponent } from './bot-chat/bot-chat.component';
 import { ChatHeadingComponent } from './chat-heading/chat-heading.component';
@@ -15,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { CustomerChatComponent } from './components/customer-chat/customer-chat.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +29,8 @@ import { FooterComponent } from './components/layout/footer/footer.component';
     LandingComponent,
     CustomerChatComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- allisn/src/app/text-scraper.service.ts
+++ allisn/src/app/text-scraper.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 
 import { Observable, of } from 'rxjs';
 
+import { MessageService } from './message.service';
+//import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { badWord } from './badWord';
 import { BADWORDS } from './mock-badWords';
 
@@ -13,10 +16,17 @@ import { BADWORDS } from './mock-badWords';
 
 export class TextScraperService {
 
-  constructor() { }
+  constructor(
+    /*private http: HttpClient,*/
+    private messageService: MessageService
+  ) { }
 
   getBadWords(): Observable<badWord[]> {
+    //return this.http.get<badWord[]>("")
+    this.messageService.add('TextScraperService: fetched badWords');
 		return of(BADWORDS);
   }
+
+
 
 }
