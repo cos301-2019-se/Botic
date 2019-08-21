36486c43cff2b311f5672ece009e8f225275b17e
Justin Grenfell <justin.grenfell110@gmail.com>
2019-08-21 17:22:04 +0200

    Added new word exception updated requirements
    Removed redundant files as well

--- ai-message_scrubber/app.py
+++ ai-message_scrubber/app.py
@@ -165,6 +165,7 @@ def parseInfo(info):
 			else:
 				changed += "," + addEntry(infoArray.index(word), (severityIndex))
 
+
 		#Third, check for names
 		#TODO: Change this to work with NLTK
 		#for y in infoArray:
@@ -173,12 +174,13 @@ def parseInfo(info):
 		#            changed += addEntry(str(infoArray.index(y)), '0')
 
 		#Forth, check for important date
-		if is_date(word):
-			if first == True:
-				changed += addEntry(str(infoArray.index(word)), '0')
-				first = False
-			else:
-				changed += "," + addEntry(str(infoArray.index(word)), '0')
+		if not(len(word) > 10):
+			if is_date(word):
+				if first == True:
+					changed += addEntry(str(infoArray.index(word)), '0')
+					first = False
+				else:
+					changed += "," + addEntry(str(infoArray.index(word)), '0')
 
 		#Fifth, check for words you haven't seen before
 		if inDictionary(dictionary, word) == False:
@@ -193,68 +195,6 @@ def parseInfo(info):
 
 	changed += "]"
 	return changed
-
-#@return: a string containing word indexes(as if the string was an array) which reveal private info
-# followed by a ':' and then the severity of the PI with 3 as cant send and 1 as ok but weird o.O
-def check(input_data):	#Reads json file of predefined triggers and checks input against them, to be replaced with word2vec/Neural Network
-	with open('flags.txt', 'r') as file:
-		data_raw = file.read()
-	data = json.loads(data_raw)
-	result = []
-	severity_index = []
-
-	input_data = input_data.lower()
-	for x in data["identifiers"]:
-		severity_count = 0
-		for y in data["privacy_type"]:
-			check_for = y + x
-			check_for = check_for.lower()
-			if check_for in input_data:
-				temp = wordIndex(input_data, input_data.index(check_for) + len(check_for))
-				if temp not in result:
-					severity_index.append(data["severity_index"][severity_count])
-					result.append(temp)
-			severity_count += 1
-
-	result = repr(result)
-	severity_count = 0
-	i = 0
-	if len(severity_index) != 0:
-		while i < len(result):
-			if result[i] == ',' or result[i] == ']':
-				result = result[0: i] + ":" + str(severity_index[severity_count]) + result[i:]
-				severity_count += 1
-				i += 2
-			i += 1
-	return result
-
-def wordIndex(input_data, char_index):
-	input_data += ' '
-	count = char_index + 1
-	personal_info = ''
-	while True:
-		if input_data[count] == ' ':
-			count = 0
-			words = input_data.split(' ')
-			for x in words:
-				if personal_info != x:
-					count += 1
-				else:
-					return count
-		else:
-			personal_info += input_data[count]
-			count += 1
-
-def formatReturn(data):
-	jsonObj = "["
-	temp = data[1:-1].replace(" ", "").split(',')
-	for x in temp:
-		y = x.split(':')
-		jsonObj += "{\'position\':" + y[0] + ","
-		jsonObj += "\'severity\':" + y[1] + "},"
-	jsonObj = jsonObj[0:-1] + "]"
-	return jsonObj
-
 
 #initialise the model and the dictionary
 dictionary = arrayFromFile("dictionaries/20k.txt")
