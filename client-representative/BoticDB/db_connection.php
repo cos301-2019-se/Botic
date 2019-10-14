cd42be6740d6888d6141b0619a12ad5edf4c748e
Kyle <kyl3var@gmail.com>
2019-10-14 07:26:48 +0200

    Updated SQL
    Changed from mysqli to pg

--- client-representative/BoticDB/db_connection.php
+++ client-representative/BoticDB/db_connection.php
@@ -1,18 +1,16 @@
 <?php
-function OpenCon()
- {
- $dbhost = "sql9.freesqldatabase.com";
- $dbuser = "sql9302125";
- $dbpass = "zriBQtNF5Q";
- $db = "sql9302125";
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
- 
- return $conn;
- }
- 
-function CloseCon($conn)
- {
-    $conn -> close();
- }
-   
+  function pg_connection_string() {
+    return "dbname=detua3tvjjqr5k host=ec2-174-129-10-235.compute-1.amazonaws.com port=5432 user=cyklqspuqrlzeo password=36b4c5aef1f947c29e90e87595329a51989965369a799d0166f09915f3eb118d sslmode=require";
+  }
+
+  function openCon() {
+    # Establish db connection
+    $db = pg_connect(pg_connection_string());
+    if (!$db) {
+      echo "Database connection error.";
+      exit;
+    }
+    else echo "Connected to Database!";
+    return $db;
+  }
 ?>
--- client-representative/BoticDB/sendMail.php
+++ client-representative/BoticDB/sendMail.php
@@ -1,25 +1,24 @@
 <?php
    // the message
    include 'db_connection.php';
-   $conn = OpenCon();
+   $db = openCon();
 
-   $sql = "SELECT * FROM ForwardedMessages WHERE Status = 100 ORDER BY id DESC";
-   $result = mysqli_query($conn, $sql);
-   if (mysqli_num_rows($result) > 0) {
-      $row = mysqli_fetch_assoc($result);
-      $email = $row["Contact"];
+   $sql = "SELECT * FROM ForwardedMessages WHERE status = 100 ORDER BY id DESC";
+   $result = pg_query($db, $sql);
+   if (pg_num_rows($result) > 0) {
+      $row = pg_fetch_assoc($result);
+      $email = $row["contact"];
       $id1 = $row["id"];
-      $subject = "RE: " . $row["Subject"];
+      $subject = "RE: " . $row["subject"];
    } else {
        echo "0 results";
    }
-   CloseCon($conn);
+   pg_close($db);
 
    ini_set('display_errors', 1);
    error_reporting(E_ALL);
    $msg = $_POST['inputField'];
    $msg = wordwrap($msg, 70, "\r\n");
-   // $msg .= " Sent from BOTIC - The Privacy Aware Chatbot";
    ini_set("SMTP", "aspmx.l.google.com");
    ini_set("sendmail_from", $email);   
    $headers = "From: noreply@Botic.co.za";
@@ -27,10 +26,10 @@
    mail($email, $subject, $msg, $headers);
    header('Location: index.php');
 
-   $conn = OpenCon();
+   $db = openCon();
    $closeTime = microtime(true);
 
-   $sql = "UPDATE ForwardedMessages SET Status = 200, TimeOut = NOW() WHERE id = " . $id1;
-   mysqli_query($conn, $sql);
-   CloseCon($conn);
+   $sql = "UPDATE ForwardedMessages SET status = 200, timeout = NOW() WHERE id = " . $id1;
+   pg_query($db, $sql);
+   pg_close($db);
 ?>
