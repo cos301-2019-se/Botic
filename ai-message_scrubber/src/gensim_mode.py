a955daaf025bd5db475f17398bb6d3e3d77040cc
Peter <u13042352@tuks.co.za>
2019-10-16 21:12:54 +0200

    Retrained model.

--- ai-message_scrubber/src/gensim_mode.py
+++ ai-message_scrubber/src/gensim_mode.py
@@ -11,7 +11,7 @@ def arrayFromFile(filename):
     text_file.close()
     return lines
 
-dataset = arrayFromFile("../dictionaries/dataset.txt")
+dataset = arrayFromFile("../dataset/dataset.txt")
 
 # show the example of list of list format of the custom corpus for gensim modeling
 #print(sent[:2])
